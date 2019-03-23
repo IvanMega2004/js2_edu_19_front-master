f878de899b95ea0d282f9dbfa34bf2c4f4f33018
Olga Novik <olga.v.novik@gmail.com>
2019-03-23 15:09:13 +0300

    Created store for news

--- src/store.js
+++ src/store.js
@@ -4,7 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    news: [
+      {
+        title: "Хранилище",
+        text: "Ура, сложная задача",
+        img: ""
+
+      }
+    ]
+  },
+  mutations: {
+    setNews(state, news) {
+      state.news = news
+    }
+  },
+  actions: {
+    addNews({ state, commit }, item) {
+let newItem = state.news.concat();
+newItem.push(item);
+commit("setNews", newItem);
+    }
+  }
 });
